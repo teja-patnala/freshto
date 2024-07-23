e0f82da111531f481a66852b8a795c87df8d0f4e
teja_patnala <patnalatejaa@gmail.com>
2024-07-23 21:11:00 +0530

    first

---
+++ src/components/Home/index.js
@@ -0,0 +1,113 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './index.css';
+import freshTo from "../../assets/fresto.jpg";
+import fruitVideo from "../../assets/videos/fruits.mp4";
+import milkVideo from "../../assets/videos/MilkVideo.mp4";
+import groceries from "../../assets/videos/groceries.mp4";
+
+const updateElementAttributes = () => {
+    const elements = document.querySelectorAll('.animate-element');
+    let angle, distance;
+
+    elements.forEach(element => {
+        const screenWidth = window.innerWidth;
+
+        if (screenWidth > 1024) {
+            angle = parseInt(element.getAttribute('data-angle-desktop')) || 0;
+            distance = parseInt(element.getAttribute('data-distance-desktop')) || 0;
+        } else if (screenWidth > 768) {
+            angle = parseInt(element.getAttribute('data-angle-tablet')) || 0;
+            distance = parseInt(element.getAttribute('data-distance-tablet')) || 0;
+        } else if (screenWidth > 479) {
+            angle = parseInt(element.getAttribute('data-angle-mobile')) || 0;
+            distance = parseInt(element.getAttribute('data-distance-mobile')) || 0;
+        } else {
+            angle = parseInt(element.getAttribute('data-angle-mobile-m')) || 0;
+            distance = parseInt(element.getAttribute('data-distance-mobile-m')) || 0;
+        }
+
+        const radian = angle * (Math.PI / 180);
+        const x = distance * Math.cos(radian);
+        const y = distance * Math.sin(radian);
+        element.style.transform = `translate(${x}px, ${y}px)`;
+    });
+};
+
+export default function Home() {
+    useEffect(() => {
+        updateElementAttributes();
+
+        window.addEventListener('resize', updateElementAttributes);
+
+        return () => {
+            window.removeEventListener('resize', updateElementAttributes);
+        };
+    }, []);
+
+    return (
+        <div className="home-container">
+            <div className='card-11 animate-element'
+                 data-angle-desktop="203" data-distance-desktop="400"
+                 data-angle-tablet="0" data-distance-tablet="0"
+                 data-angle-mobile-m="0" data-distance-mobile-m="0"
+                 data-angle-mobile="0" data-distance-mobile="0">
+                <button className="buy-button">Our Services</button>
+                <h4 className='card-11-heading'>Order fruits, groceries—we deliver quality products to your doorstep.</h4>
+            </div>
+            <div className="flex-container">
+                <div className="sub-container sub-container-1">
+                    <img className='fresto-img' src={freshTo} alt="freshto" />
+                    <div className='card-1 animate-element' 
+                         data-angle-desktop="120" data-distance-desktop="180"
+                         data-angle-tablet="87" data-distance-tablet="120"
+                         data-angle-mobile-m="90" data-distance-mobile-m="-50"
+                         data-angle-mobile="90" data-distance-mobile="-90">
+                        <h1 id ="font-size-card-i" className='special-heading'>Today's Special Picks</h1>  
+                    </div>
+                    <div className='card-1 animate-element' 
+                         data-angle-desktop="160" data-distance-desktop="450"
+                         data-angle-tablet="150" data-distance-tablet="300"
+                         data-angle-mobile-m="90" data-distance-mobile-m="490"
+                         data-angle-mobile="90" data-distance-mobile="310">
+                        <video className="fruit-video" src={groceries} autoPlay loop muted playsInline />
+                        <Link to="/visit-store" className="buy-button">Visit Store</Link>
+                    </div>
+                    <div className='card-1 animate-element'
+                         data-angle-desktop="180" data-distance-desktop="330"
+                         data-angle-tablet="180" data-distance-tablet="260"
+                         data-angle-mobile-m="90" data-distance-mobile-m="330"
+                         data-angle-mobile="90" data-distance-mobile="200">
+                        <video className="fruit-video" src={milkVideo} autoPlay loop muted playsInline />
+                        <Link to="/milk-plan" className="buy-button">Milk Plan</Link>
+                    </div>
+                    <div className='card-1 animate-element' 
+                         data-angle-desktop="235" data-distance-desktop="190"
+                         data-angle-tablet="260" data-distance-tablet="140"
+                         data-angle-mobile-m="90" data-distance-mobile-m="170"
+                         data-angle-mobile="90" data-distance-mobile="90">
+                        <video className="fruit-video" src={fruitVideo} autoPlay loop muted playsInline />
+                        <Link to="/buy-fruits" className="buy-button">Buy Fruits</Link>
+                    </div>
+                </div>
+                <div className="sub-container sb-v-1 sub-container-2">
+                    <div className="nav-section">
+                        <h3 id="font-size-card" className='special-heading'>Explore Our Exclusive Offers</h3>
+                        <Link to="/fruit-store" className="nav-button n-v-1">
+                            <h4>Fruit Store</h4>
+                            <p>Fresh, juicy fruits picked just for you. Explore our wide range of seasonal and exotic fruits.</p>
+                        </Link>
+                        <Link to="/kirana-store" className="nav-button n-v-1">
+                            <h4>Kirana Store</h4>
+                            <p>Your one-stop shop for all daily essentials and groceries. Quality products at unbeatable prices.</p>
+                        </Link>
+                        <Link to="/milk-subscription" className="nav-button n-v-1">
+                            <h4>Milk Subscription</h4>
+                            <p>Enjoy fresh milk delivered to your doorstep. Choose from various plans tailored to your needs.</p>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import './index.css';
+import { Link } from "react-router-dom";
+import img1 from '../../assets/logoo.jpg';
+import homeIcon from "../../assets/icons/house-chimney.svg";
+import galleryIcon from "../../assets/icons/gallery.svg";
+import aboutIcon from "../../assets/icons/info.svg";
+import userIcon from "../../assets/icons/user-pen.svg";
+import menuIcon from "../../assets/icons/apps.svg";
+import basketIcon from "../../assets/icons/shopping-basket.svg";
+import ordersIcon from "../../assets/icons/list.svg";
+import editIcon from "../../assets/icons/pen-nib.svg";
+
+export default function Navbar() {
+  const [homeIconState, setHomeIconState] = useState(false);
+  const [galleryIconState, setGalleryIconState] = useState(false);
+  const [aboutIconState, setAboutIconState] = useState(false);
+  const [hovered, setHovered] = useState(false);
+  const [menuState, setMenuState] = useState(false);
+
+  return (
+    <div className="navbar">
+      <img alt="logo" className="logo" src={img1} />
+      <div className="mid-nav-one">
+        <Link className='nav-link' to="/">
+          <div
+            onMouseEnter={() => setHomeIconState(true)}
+            onMouseLeave={() => setHomeIconState(false)}
+            className="mid-nav-one-i"
+          >
+            <img className="nav-icon" src={homeIcon} alt="home-icon" />
+            <p className={`icon-name ${homeIconState ? "anmt-icon" : ""}`}>
+              {homeIconState ? "Home" : ""}
+            </p>
+          </div>
+        </Link>
+        <Link className='nav-link' to="/gallery">
+          <div
+            onMouseEnter={() => setGalleryIconState(true)}
+            onMouseLeave={() => setGalleryIconState(false)}
+            className="mid-nav-one-i"
+          >
+            <img className="nav-icon" src={galleryIcon} alt="gallery-icon" />
+            <p className={`icon-name ${galleryIconState ? "anmt-icon" : ""}`}>
+              {galleryIconState ? "Gallery" : ""}
+            </p>
+          </div>
+        </Link>
+        <Link className='nav-link' to="/about">
+          <div
+            onMouseEnter={() => setAboutIconState(true)}
+            onMouseLeave={() => setAboutIconState(false)}
+            className="mid-nav-one-i"
+          >
+            <img className="nav-icon" src={aboutIcon} alt="about-icon" />
+            <p className={`icon-name ${aboutIconState ? "anmt-icon" : ""}`}>
+              {aboutIconState ? "About" : ""}
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div
+        className="mid-nav-two"
+        onMouseEnter={() => setMenuState(true)}
+        onMouseLeave={() => setMenuState(false)}
+      >
+        <img
+          className={`mid-nav-icon menu-icon ${menuState ? 'menu-move-down' : ''}`}
+          src={menuIcon}
+          alt="menu-icon"
+        />
+        <Link className={`mid-nav-icon nav-background ${menuState ? 'menu-move-down' : 'hide'}`} to="/">
+          <div
+            onMouseEnter={() => setHomeIconState(true)}
+            onMouseLeave={() => setHomeIconState(false)}
+            className="mid-nav-one-i"
+          >
+            <img className="nav-icon" src={homeIcon} alt="home-icon" />
+            <p className={`icon-name ${homeIconState ? "anmt-icon" : ""}`}>
+              {homeIconState ? "Home" : ""}
+            </p>
+          </div>
+        </Link>
+        <Link className={`mid-nav-icon nav-background ${menuState ? 'menu-move-down' : 'hide'}`} to="/gallery">
+          <div
+            onMouseEnter={() => setGalleryIconState(true)}
+            onMouseLeave={() => setGalleryIconState(false)}
+            className="mid-nav-one-i"
+          >
+            <img className="nav-icon" src={galleryIcon} alt="gallery-icon" />
+            <p className={`icon-name ${galleryIconState ? "anmt-icon" : ""}`}>
+              {galleryIconState ? "Gallery" : ""}
+            </p>
+          </div>
+        </Link>
+        <Link className={`mid-nav-icon nav-background ${menuState ? 'menu-move-down' : 'hide'}`} to="/about">
+          <div
+            onMouseEnter={() => setAboutIconState(true)}
+            onMouseLeave={() => setAboutIconState(false)}
+            className="mid-nav-one-i"
+          >
+            <img className="nav-icon" src={aboutIcon} alt="about-icon" />
+            <p className={`icon-name ${aboutIconState ? "anmt-icon" : ""}`}>
+              {aboutIconState ? "About" : ""}
+            </p>
+          </div>
+        </Link>
+      </div>
+
+      <div>
+        <img className="nav-icon" src={basketIcon} alt="basket-icon" />
+      </div>
+      <div
+        className='icon-stack'
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
+        <div className={`icon ${hovered ? 'move-down' : ''}`}>
+          <img id="profile" className="user-icon" src={userIcon} alt="user-icon" />
+          <p className='profile-p'>Profile</p>
+        </div>
+        <div className={`icon ${hovered ? 'move-down' : 'hide'}`}>
+          <Link to ="edit-profile" id="icon-box">
+            <img id="user-icon" src={editIcon} alt="edit-icon" />
+            <p className='profile-p'>Edit</p>
+          </Link>
+        </div>
+        <div className={`icon ${hovered ? 'move-down' : 'hide'}`}>
+          <Link to ="/orders" id="icon-box">
+            <img id="user-icon" src={ordersIcon} alt="orders-icon" />
+            <p className='profile-p para'>Orders</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
